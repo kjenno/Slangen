e22e42a5bf81abafab0ea4db0c9387c412e75dab
kjenno <507598@vistacollege.nl>
2022-11-17 10:42:41 +0100

    update

---
+++ delen.py
@@ -0,0 +1,5 @@
+nr1 = float(input('kies nummer 1. '))
+nr2 = float(input('kies nummer 2. '))
+nr3 = float(input('kies nummer 2. '))
+antwoord = float(input(round(nr1 / nr2 / nr3, 4)))
+
---
+++ rijbewijs.py
@@ -0,0 +1,8 @@
+A = input("leeftijd:")
+B = 18
+A = int(A)
+if A > B:
+    print("je mag rijden")
+
+if A < B:
+    print("je mag niet rijden")
