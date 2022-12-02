124661457df7225090ff407ddab4f14174dfb7d4
kjenno <507598@vistacollege.nl>
2022-12-02 09:39:07 +0100

    yes

---
+++ bezig/sinterklaas.py
@@ -0,0 +1,7 @@
+namen = []
+for A in range(100000):
+    B = input('cadeau: ')
+    namen.append(B)
+    if B == "KLAAR":
+        namen.sort
+        print(namen)
--- nagekeken/wie mag er beginnen.py
+++ nagekeken/wie mag er beginnen.py
@@ -1,7 +1,7 @@
 import random
 namen = []
 D = int(input("Aantal spelers: "))
-for B in range(D):
+for i in range(D):
     B = input('naam: ')
     namen.append(B)
 print(random.choice(namen))
